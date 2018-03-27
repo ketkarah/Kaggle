2f0df5804e07ca2f03ba6d5f4fd6756e98348a5a
Akshay Ketkar <ketkarah@gmail.com>
2018-03-27 09:07:54 -0500

    Delete Ensemble.py

--- KaggleToxicText/Ensemble.py
+++
@@ -1,7 +0,0 @@
-import numpy as np, pandas as pd
-df_logistic=pd.read_csv('C:/Users/ketka/Desktop/kaggle/submission.csv')
-df_xgb=pd.read_csv('C:/Users/ketka/Desktop/kaggle/submission1.csv')
-labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-df_ensemble=df_xgb.copy()
-df_ensemble[labels]=df_logistic[labels]*0.6 + df_xgb[labels]*0.4
-df_ensemble.to_csv('C:/Users/ketka/Desktop/kaggle/output.csv',index=False)
